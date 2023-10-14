4f0ea4d9bdd520f782442198890866cbec2137f3
lior <liorh@elad.co.il>
2023-10-14 16:05:35 +0300

    prettier

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import css from './Login.module.scss';
-import { blueGrey } from '@mui/material/colors';
-import { Typography } from '@mui/material';
+import React from "react";
+import Box from "@mui/material/Box";
+import css from "./Login.module.scss";
+import { blueGrey } from "@mui/material/colors";
+import { Typography } from "@mui/material";
 
 export const Login = () => {
-    return <Box className={css.wrapper} sx={{
-        bgcolor: blueGrey[50]
-    }}>
-        <Box className={css.main}>
-            <Typography
-                className={css.logo} variant='h4' component='h1'>
-                Friends.
-            </Typography>
-        </Box>
+  return (
+    <Box
+      className={css.wrapper}
+      sx={{
+        bgcolor: blueGrey[50],
+      }}
+    >
+      <Box className={css.main}>
+        <Typography className={css.logo} variant="h4" component="h1">
+          Friends.
+        </Typography>
+      </Box>
     </Box>
-}
+  );
+};
