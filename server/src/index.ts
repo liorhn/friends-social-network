b370109c9800c1163ea1845d1d97a63f51d7fa40
lior <liorh@elad.co.il>
2023-10-16 01:20:28 +0300

    server webpack

---
+++ server/src/index.ts
@@ -0,0 +1,10 @@
+import * as express from 'express';
+
+const app = express();
+
+app.get('/login', (req, res) => {
+    res.send('Welcome to Login page!');
+})
+app.listen(3000, () => {
+    console.log('Running on server 3000!');
+});
