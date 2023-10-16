9c11bf216edb329d6b5c02c319e038b7acb2ccbf
lior <liorh@elad.co.il>
2023-10-16 21:50:55 +0300

    server eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,8 +3,8 @@ import * as express from 'express';
 const app = express();
 
 app.get('/login', (req, res) => {
-    res.send('Welcome to Login pagesadasd!');
-})
+  res.send('Welcome to Login pagesadasd!');
+});
 app.listen(3000, () => {
-    console.log('Running on server 3000!');
+  console.log('Running on server 3000!');
 });
