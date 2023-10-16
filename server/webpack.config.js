672d16959118b989e8bd2042a87a0e8c6f11d9fc
lior <liorh@elad.co.il>
2023-10-16 21:58:54 +0300

    webpack config allow require

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const NodemonPlugin = require('nodemon-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
